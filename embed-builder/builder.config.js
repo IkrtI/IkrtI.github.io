19d61e7a34e29bd0b579ceeebfa337d2c371e113
No_One <57593203+miniOPXD@users.noreply.github.com>
2022-04-16 00:05:53 +0700

    embed-builder
    by https://github.com/Glitchii/embedbuilder

---
+++ embed-builder/builder.config.js
@@ -0,0 +1,31 @@
+// URL options can override the options below.
+// Options set through the menu can override both.
+options = {
+    username:           'Discord Bot',
+    avatar:             'https://cdn.discordapp.com/embed/avatars/1.png',
+    verified:           false,
+    noUser:             false,
+    dataSpecified:      false,
+    guiTabs:            ['author', 'description'],
+    useJsonEditor:      false,
+    reverseColumns:     false,
+    allowPlaceholders:  false,
+    autoUpdateURL:      false,
+    autoParams:         false,
+    hideEditor:         false,
+    hidePreview:        false,
+    hideMenu:           false,
+}
+
+// // Default JSON object
+// json = {
+//     content: "Hello world",
+//     embed: {
+//         title: "A title",
+//         description: "A description",
+//     }
+// }
+
+onload = () => {
+    // console.log('Salut 👋');
+}
